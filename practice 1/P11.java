f941bdc828e9795298776c3f6c2d8a0ac8c8e66f
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-15 21:12:34 +0545

    add p6-p13

---
+++ practice 1/P11.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class P11 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        
+        int n;
+        System.out.println("Enter a number " );
+        n=scan.nextInt();
+        
+        if(n<10)
+            if(n>0)
+                System.out.println("The number is positive.");
+            else
+                System.out.println("The number is negative");
+            
+        }
+    }
+ 
+
---
+++ practice 1/P8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class P8 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String message;
+        System.out.println("Enter a sentence on the line below.");
+       // message=scan.next();
+        message=scan.nextLine();
+        System.out.println(message);
+
+    }
+    
+}
