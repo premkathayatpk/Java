3ed24c1be00d5e9dc8745bf24fff128805e28b4d
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-15 21:11:37 +0545

    add j

---
+++ practice 1/Odd10.java
@@ -0,0 +1,15 @@
+
+public class Odd10 {
+    public static void main(String[] args) {
+       
+        for(int i=20;i>0;i--){
+            if(i%2==1){
+             System.out.println("Even Reverse order: "+i);
+
+            }
+        }}
+    }
+    
+
+
+
---
+++ practice 1/P4.java
@@ -0,0 +1,11 @@
+
+public class P4 {
+    enum colorType {red, orange, yellow, green, blue, violet};
+public static void main(String[] args)
+{
+    colorType shirt, pants;
+    shirt = colorType.red;
+    pants = colorType.blue;
+    System.out.println("  " + shirt + "  " + pants );
+}
+}
