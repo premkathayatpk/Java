c11ff1665e7ea02878a21930f566b6c948fd37a9
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-18 22:08:10 +0545

    update p6

--- practice1/P6.java
+++ practice1/P6.java
@@ -1,10 +1,12 @@
-//
-//public class P6 {
-//    public static void main(String[] args) {
-//        int found=0,count=5;
-//        if (!found || ‐‐count == 0)
-//    System.out.println( "danger" );
-//        System.out.println("count = "+count);
-//    }
-// 
-//}
+public class P6 {
+    public static void main(String[] args) {
+        boolean found = false; // Correctly initializing found as a boolean
+        int count = 5;
+
+        if (!found || --count == 0) {
+            System.out.println("danger");
+        }
+
+        System.out.println("count = " + count);
+    }
+}
