68db00e77a0b8af041aab6f26e47707adbb44bbb
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-22 20:08:30 +0545

    update

--- practice1/P30.java
+++ practice1/P30.java
@@ -1,5 +1,13 @@
 public class P30 {
+    public static float sum(float[] arr){
+        float sum=0f;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-        System.out.println("Prem");
+    float[] arr={5.8f,2.6f,9f,3.4f,7.1f};
+        System.out.println("sum of array is "+sum(arr));
     }
 }
