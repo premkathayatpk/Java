29835a4657e70334ea4efda1eb30c01d7e1291c7
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-25 18:10:02 +0545

    reverse array

---
+++ DSA Array/src/ReversArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ReversArray {
+    public static int[] reverse(int[] arr){
+        int[] rev=new int[arr.length];
+        int j=0;
+      for(int i=arr.length-1;i>=0;i--){
+          rev[j]= arr[i];
+          j++;
+      }
+
+       return rev;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,3,2,3};
+        int[] reversedArr = reverse(arr);
+        System.out.println(Arrays.toString(reversedArr));
+    }
+}
+
