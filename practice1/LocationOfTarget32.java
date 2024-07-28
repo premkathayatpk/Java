c7707cd6afb73d08e5cb7880427f285e82a00a91
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-07-28 17:49:15 +0545

    add LocationOfTarget32.java

---
+++ practice1/LocationOfTarget32.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class LocationOfTarget32{
+  static int locationOfTarget(int[] arr, int target){
+     int i;
+    int best=0;
+    for( i=0;i<arr.length;i++){
+      if(arr[i]==target){
+        best=i;
+      
+      }    
+   
+  }
+   return best;
+
+  }
+
+  public static void main(String[] args){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter size of array.");
+    int size=scan.nextInt();
+    int[] arr=new int[size];
+    System.out.println("Enter"+ size +" elements of array.");
+    for(int i=0;i<size;i++){
+      arr[i]=scan.nextInt();
+    }
+    System.out.println("Enter target value.");
+    int target=scan.nextInt();
+    
+     System.out.println("Location of Largest is "+locationOfTarget(arr,target));
+
+  }
+}
