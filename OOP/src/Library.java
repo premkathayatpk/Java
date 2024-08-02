529c420de598168e5bac7c80d0c3c5fc419aa25c
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-08-02 20:29:10 +0545

    test prime

--- OOP/src/Library.java
+++ OOP/src/Library.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Library {
+
+class Book {
+    static int totalBook;
+    String title;
+    String auther;
+    String isbn;
+    boolean isBorrowed;
+    static {
+        totalBook = 0;
+
+        // Object Init
+        totalBook++;
+    }
+
+    Book(String title, String auther, String isbn) {
+        this.title = title;
+        this.auther = auther;
+        this.isbn = isbn;
+    }
+
+    Book(String isbn) {
+        this(isbn, "Unknown", "Unknown");
+    }
+
+    static int getTotalBook() {
+        return totalBook;
+    }
+
+    public void borrowBook() {
+        if (isBorrowed) {
+            System.out.println("It is already borrowed.");
+        } else {
+            this.isBorrowed = true;
+            System.out.println("Enjoy the book");
+        }
+    }
+
+    public void returnBook() {
+        if (isBorrowed) {
+            this.isBorrowed = false;
+            System.out.println("Hope you enjoy ");
+        } else {
+            System.out.println("This book is already in the liabarary.");
+        }
+    }
+
+}
+
+public class Library {
+    public static void main(String[] args) {
+        Book b = new Book("Math", "Ram", "123");
+        Book b2 = new Book("Math", "Ram", "123");
+
+        b.borrowBook();
+        b.borrowBook();
+        b.returnBook();
+        b.returnBook();
+        System.out.println(b.getTotalBook());
+
+    }
+
 }
